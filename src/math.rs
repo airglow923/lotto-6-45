22d03d89e23bc2648ad4f9884bc146d8b856182d
airglow923 <p.hyundeok76@gmail.com>
2025-10-17 00:36:30 +0900

    feat: make fixed set optional for combination generation test: add test cases for binomial coefficient and combinations

--- src/math.rs
+++ src/math.rs
@@ -28,15 +28,20 @@ pub fn binomial_coefficient(numerator: usize, denominator: usize) -> usize {
 }
 
 pub fn combinations<T: Default + Copy>(
-    set: &Vec<T>,
-    set_fixed: &Vec<T>,
+    set: &[T],
+    set_fixed: Option<&[T]>,
     n_seq: usize,
-) -> Result<Vec<Vec<T>>, ()> {
+) -> Result<Vec<Vec<T>>, String> {
     let n_set = set.len();
-    let n_fixed = set_fixed.len();
+    let n_fixed = match set_fixed {
+        Some(t) => t.len(),
+        None => 0,
+    };
 
     if n_set == 0 || n_seq <= n_fixed {
-        return Err(());
+        return Err(
+            "set cannot be empty and n_seq should be greater than length of set_fixed".to_owned(),
+        );
     }
 
     let n_rand = n_seq - n_fixed;
@@ -48,8 +53,10 @@ pub fn combinations<T: Default + Copy>(
         let mut comb: Vec<T> = Vec::new();
         comb.reserve_exact(n_seq);
 
-        for v in set_fixed {
-            comb.push(*v);
+        if n_fixed != 0 {
+            for v in set_fixed.unwrap() {
+                comb.push(*v);
+            }
         }
 
         for v in set_comb {
@@ -60,4 +67,129 @@ pub fn combinations<T: Default + Copy>(
     }
 
     Ok(combs)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_binomial_coefficient_tn_zero_numerator() {
+        let ret = binomial_coefficient(0, 1);
+
+        assert_eq!(ret, 0);
+    }
+
+    #[test]
+    fn test_binomial_coefficient_tp_zero_denominator() {
+        let ret = binomial_coefficient(1, 0);
+
+        assert_eq!(ret, 1);
+    }
+
+    #[test]
+    fn test_binomial_coefficient_tp_equal_n_k() {
+        let ret = binomial_coefficient(1, 1);
+
+        assert_eq!(ret, 1);
+    }
+
+    #[test]
+    fn test_binomial_coefficient_tn_greater_k() {
+        let ret = binomial_coefficient(1, 2);
+
+        assert_eq!(ret, 0);
+    }
+
+    #[test]
+    fn test_binomial_coefficient_tp_valid() {
+        let ret = binomial_coefficient(45, 6);
+
+        assert_eq!(ret, 8145060);
+    }
+
+    #[test]
+    fn test_combinations_tp_valid_slice() {
+        let ret = combinations(&[1, 2, 3, 4, 5], Some(&[6, 7]), 3);
+        assert!(ret.is_ok());
+
+        let combs = ret.unwrap();
+        assert_eq!(
+            combs,
+            vec![
+                vec![6, 7, 1],
+                vec![6, 7, 2],
+                vec![6, 7, 3],
+                vec![6, 7, 4],
+                vec![6, 7, 5],
+            ],
+        );
+        assert_eq!(combs.len(), binomial_coefficient(5, 1));
+        assert_eq!(combs.capacity(), binomial_coefficient(5, 1));
+        assert_eq!(combs[0].len(), 3);
+        assert_eq!(combs[0].capacity(), 3);
+    }
+
+    #[test]
+    fn test_combinations_tp_valid_vec() {
+        let ret = combinations(&vec![1, 2, 3, 4, 5], Some(&vec![6, 7]), 3);
+        assert!(ret.is_ok());
+
+        let combs = ret.unwrap();
+        assert_eq!(
+            combs,
+            vec![
+                vec![6, 7, 1],
+                vec![6, 7, 2],
+                vec![6, 7, 3],
+                vec![6, 7, 4],
+                vec![6, 7, 5],
+            ],
+        );
+        assert_eq!(combs.len(), binomial_coefficient(5, 1));
+        assert_eq!(combs.capacity(), binomial_coefficient(5, 1));
+        assert_eq!(combs[0].len(), 3);
+        assert_eq!(combs[0].capacity(), 3);
+    }
+
+    #[test]
+    fn test_combinations_tp_no_fixed() {
+        let ret = combinations(&[1, 2, 3, 4, 5], None, 3);
+        assert!(ret.is_ok());
+
+        let combs = ret.unwrap();
+        assert_eq!(
+            combs,
+            vec![
+                vec![1, 2, 3],
+                vec![1, 2, 4],
+                vec![1, 2, 5],
+                vec![1, 3, 4],
+                vec![1, 3, 5],
+                vec![1, 4, 5],
+                vec![2, 3, 4],
+                vec![2, 3, 5],
+                vec![2, 4, 5],
+                vec![3, 4, 5],
+            ],
+        );
+        assert_eq!(combs.len(), binomial_coefficient(5, 3));
+        assert_eq!(combs.capacity(), binomial_coefficient(5, 3));
+        assert_eq!(combs[0].len(), 3);
+        assert_eq!(combs[0].capacity(), 3);
+    }
+
+    #[test]
+    fn test_combinations_tp_45_6() {
+        let ret = combinations(&(1..=45).collect::<Vec<_>>(), None, 6);
+        assert!(ret.is_ok());
+
+        let combs = ret.unwrap();
+        assert_eq!(combs.len(), binomial_coefficient(45, 6));
+        assert_eq!(combs.capacity(), binomial_coefficient(45, 6));
+        assert_eq!(combs[0].len(), 6);
+        assert_eq!(combs[0].capacity(), 6);
+
+        println!("{:?}", &combs[..10]);
+    }
 }
